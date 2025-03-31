9083a0fb89e8467786e7a3b5304ecbb0917af667
LAGTactico7 <lagtactico@gmail.com>
2025-03-31 17:22:11 -0300

    Modificando rutas install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,9 @@ echo ""
 [ -d ".config" ] && cp -r .config/* ~/.config/ && echo "Archivos de .config copiados a ~/.config." || echo "No se encontró .config."
 [ -d ".fonts" ] && cp -r .fonts/* ~/ && echo "Archivos de .fonts copiados a ~/" || echo "No se encontró .fonts"
 [ -d ".icons" ] && cp -r .icons/* ~/ && echo "Archivos de .icons copiados a ~/" || echo "No se encontró .icons"
-[ -d ".fonts" ] && cp -r .p10k.zsh/* ~/ && echo "Archivos de .p10k.zsh copiados a ~/" || echo "No se encontró .p10k.zsh"
-[ -d ".zshrc" ] && cp -r .zshrc/* ~/ && echo "Archivos de .zshrc copiados a ~/" || echo "No se encontró .zshrc"
-
+[ -d "wallpapers" ] && cp -r wallpapers/* ~/ && echo "Archivos de wallpapers copiados a ~/" || echo "No se encontró wallpapers"
+[ -d ".fonts" ] && cp .p10k.zsh/* ~/ && echo "Archivos de .p10k.zsh copiados a ~/" || echo "No se encontró .p10k.zsh"
+[ -d ".zshrc" ] && cp .zshrc/* ~/ && echo "Archivos de .zshrc copiados a ~/" || echo "No se encontró .zshrc"
 
 # Cambiar el shell solo si Zsh está instalado y no es el actual
 if [ "$SHELL" != "/usr/bin/zsh" ] && command -v zsh &> /dev/null; then
