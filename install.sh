255f8f7ea8d036c7c7908e1fd8d65e6398a13146
LAGTactico7 <lagtactico@gmail.com>
2025-04-03 11:42:51 -0300

    Detallito del instalador...

--- install.sh
+++ install.sh
@@ -26,21 +26,22 @@ cp -r wallpapers ~/.config/ && echo "Archivos de wallpapers copiados a ~/" || ec
 cp .p10k.zsh ~/ && echo "Archivos de .p10k.zsh copiados a ~/" || echo "No se encontró .p10k.zsh"
 cp .zshrc ~/ && echo "Archivos de .zshrc copiados a ~/" || echo "No se encontró .zshrc"
 
-if [ "$SHELL" != "/bin/zsh" ] && command -v zsh &> /dev/null; then
-    echo "Cambiando la shell a Zsh..."
-    chsh -s /bin/zsh $USER
-    echo "Reinicia sesión para aplicar los cambios."
+if [ "$SHELL" != "/usr/bin/zsh" ] && command -v zsh &> /dev/null; then
+    sudo chsh -s /usr/bin/zsh $USER
 else
     echo "Zsh ya está configurado como shell predeterminado."
 fi
 
-ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
-ZSH_PLUGIN_DIR="$ZSH_CUSTOM/plugins"
-mkdir -p "$ZSH_PLUGIN_DIR"
+if [ -z "$ZSH_CUSTOM" ]; then
+    ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+fi
 
-echo ""
+ZSH_PLUGIN_DIR="$ZSH_CUSTOM/plugins"
+ZSH_THEME_DIR="$ZSH_CUSTOM/themes"
+mkdir -p "$ZSH_PLUGIN_DIR"
+mkdir -p "$ZSH_THEME_DIR"
+
 echo "Instalando plugins de Zsh..."
-echo ""
 
 for repo in "zsh-users/zsh-autosuggestions" "zsh-users/zsh-syntax-highlighting" "chrissicool/zsh-256color"; do
     plugin_name=$(basename "$repo")
@@ -51,6 +52,14 @@ for repo in "zsh-users/zsh-autosuggestions" "zsh-users/zsh-syntax-highlighting" 
         echo "$plugin_name ya está instalado, omitiendo..."
     fi
 done
+
+if [ ! -d "$ZSH_THEME_DIR/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_THEME_DIR/powerlevel10k"
+    echo "powerlevel10k instalado."
+else
+    echo "powerlevel10k ya está instalado, omitiendo..."
+fi
+
 
 echo ""
 echo "Instalación completa. Ahora eres oficialmente gay."
